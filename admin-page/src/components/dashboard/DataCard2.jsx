4e02076f7d86979d97774bd9611f2aa298369bef
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-01-15 20:49:01 -0800

    --add: new  radial chart for the second card

--- admin-page/src/components/dashboard/DataCard2.jsx
+++ admin-page/src/components/dashboard/DataCard2.jsx
@@ -3,13 +3,67 @@ import "../../styles/dashboard.css"
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import Card from '@mui/material/Card';
+import { RadialBarChart, RadialBar, Legend } from "recharts";
 
 const SingleCard2 = (props) => {
   const { weekTotalNumber, monthTotalNumber, weekTotalNumberVerified, monthTotalNumberVerified } = props.item;
+  const data = [
+    {
+      name: "daily",
+      uv: 30,
+      fill: "#8884d8"
+    },
+    {
+      name: "Monthly",
+      uv: 160,
+      fill: "#83a6ed"
+    },
+    {
+      name: "yearly",
+      uv: 567,
+      fill: "#8dd1e1"
+    },
+    {
+      name: "varified total",
+      uv: 1154,
+      fill: "#82ca9d"
+    },
+  ];
+  const style = {
+    top: 0,
+    left: 350,
+    lineHeight: "24px"
+  };
   return (
     <Card className="single__card" sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+      <RadialBarChart
+      width={400}
+      height={300}
+      cx={150}
+      cy={150}
+      innerRadius={20}
+      outerRadius={140}
+      barSize={20}
+      data={data}
+    >
+      <RadialBar
+        minAngle={15}
+        label={{ position: "insideStart", fill: "#fff" }}
+        background
+        clockWise
+        dataKey="uv"
+      />
+      <Legend
+        iconSize={10}
+        width={120}
+        height={140}
+        layout="horizontal"
+        verticalAlign="middle"
+        wrapperStyle={style}
+      />
+    </RadialBarChart>
+        {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           New users this past week: {weekTotalNumber}
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -20,7 +74,7 @@ const SingleCard2 = (props) => {
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Soomething else about users: {monthTotalNumberVerified}
-        </Typography>
+        </Typography> */}
       </CardContent>
     </Card>
   );
--- admin-page/src/components/dashboard/DataCard3.jsx
+++ admin-page/src/components/dashboard/DataCard3.jsx
@@ -3,12 +3,22 @@ import "../../styles/dashboard.css"
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import Card from '@mui/material/Card';
+import { RadialBarChart, PolarAngleAxis, RadialBar } from 'recharts';
 
 const SingleCard3 = (props) => {
   const { weekTotalNumber, monthTotalNumber, weekTotalNumberVerified, monthTotalNumberVerified } = props.item;
+
+  // const index = () => {
+  //   const data = [
+  //     { name: "Weekly Reports", x: weekTotalNumber, fill: "#8884d8" },
+  //     { name: "Monthly Reports", x: monthTotalNumber, fill: "#83a6ed" },
+  //     { name: "Weekly Reports Verified", x: weekTotalNumberVerified, fill: "#8dd1e1" },
+  //     { name: "Monthly Reports Verified", x: monthTotalNumberVerified, fill: "#82ca9d" },
+  //   ];
   return (
     <Card className="single__card" sx={{ minWidth: 275 }}>
       <CardContent>
+
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           New reports this past week: {weekTotalNumber}
         </Typography>
